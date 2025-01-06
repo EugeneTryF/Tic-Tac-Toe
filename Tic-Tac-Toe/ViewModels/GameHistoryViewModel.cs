0de6d31d04517bfdb6ebfa522702974efad7ad75
Evgeniy Trifonov <eugenetryfanau@gmail.com>
2025-01-06 10:40:02 +0100

    Add game history and game history navigation

---
+++ Tic-Tac-Toe/ViewModels/GameHistoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tic_Tac_Toe.ViewModels
+{
+    public class GameHistoryViewModel
+    {
+        public string Description { get; set; }
+        public List<string> BoardState { get; set; } = new();
+    }
+}
